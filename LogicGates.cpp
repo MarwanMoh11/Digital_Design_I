b70ccbbe62f295ef14a549007234a7cdeb456e66
mazinbersy <124715527+mazinbersy@users.noreply.github.com>
2024-03-06 23:25:16 +0200

    Add files via upload

---
+++ LogicGates.cpp
@@ -0,0 +1,75 @@
+#include "LogicGates.h"
+
+bool LogicGates::AND(vector<bool> inputs)
+{
+    for (auto i : inputs)
+    {
+        if (!i) return false;
+   }
+    
+    return true;
+}
+
+bool LogicGates::OR(vector<bool> inputs)
+{
+    for (auto i : inputs)
+    {
+        if (i) return true;
+    }
+
+    return false;
+}
+
+bool LogicGates::NAND(vector<bool> inputs)
+{
+    for (auto i : inputs)
+    {
+        if (!i) return true;
+    }
+
+    return false;
+}
+
+bool LogicGates::NOR(vector<bool> inputs)
+{
+    for (auto i : inputs)
+    {
+        if (i) return false;
+    }
+
+    return true;
+}
+
+bool LogicGates::Buffer(bool n)
+{
+    return n;
+}
+
+bool LogicGates::NOT(bool n)
+{
+    return !n;
+}
+
+bool LogicGates::XOR(vector<bool> inputs)
+{
+    int count = 0;
+    for (auto i : inputs)
+    {
+        if (i) count++;
+    }
+
+    if (count == 1) return true;
+    return false;
+}
+
+bool LogicGates::XNOR(vector<bool> inputs)
+{
+    int count = 0;
+    for (auto i : inputs)
+    {
+        if (i) count++;
+    }
+
+    if (count == 1) return false;
+    return true;
+}
