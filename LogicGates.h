73c48df66f2f3b398102656143a69fdb4f210395
mazinbersy <124715527+mazinbersy@users.noreply.github.com>
2024-03-06 23:24:55 +0200

    Add files via upload

---
+++ LogicGates.h
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class LogicGates
+{
+public: 
+	bool AND(vector<bool> x);
+	bool OR(vector <bool>);
+	bool NAND(vector < bool>);
+	bool NOR(vector < bool>);
+	bool Buffer(bool);
+	bool NOT(bool);
+	bool XOR(vector < bool>);
+	bool XNOR(vector < bool>);
+
+};
+
